f970c5d1a2e27defae738fea15ac90655582c5d4
洛洛米 <1303081920@qq.com>
2026-08-21 10:08:02 +0800

    适配新版本角色和武器计算逻辑

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ git clone --depth=1 https://gitee.com/land-route_lu/lolomi-calc.git ./plugins/lo
 * 梁氏源地址 https://gitee.com/liangshi233/liangshi-calc/tree/master
 * 计算只适配了梁氏和喵喵，原神角色计算优先级会高于梁氏和喵喵
 * 由于miao-plugin更新实在过慢，原神7.0版本迁移miao框架的计算逻辑至lolomi，同时迁移miao-plugin的LICENSE许可文件，相关迁移文件附有注释来源说明
-* 原神7.0版本新反应公式及后续计算逻辑lolomi自行维护，计算不再依赖miao-plugin，lolomi未写角色依旧由梁氏和喵喵基础计算兜底，更新后可直接使用jx命令查看下个版本测试角色面板，前提是lolomi有写
+* 原神7.0版本新反应公式及后续计算逻辑lolomi自行维护，计算不再依赖miao-plugin，lolomi未写角色依旧由梁氏和喵喵基础计算兜底，更新后可直接使用jx命令查看下个版本测试角色面板，也可使用面板变换功能提前使用下个版本新武器，前提是lolomi有写
 # 相关依赖
 建议用 TRSS-Yunzai
 * TRSS-Yunzai   https://github.com/TimeRainStarSky/Yunzai
--- llm-models/LlmDataIndex.js
+++ llm-models/LlmDataIndex.js
@@ -3,6 +3,10 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { Data, Meta } from '../../miao-plugin/components/index.js'
+import Weapon from '../../miao-plugin/models/Weapon.js'
+import Character from '../../miao-plugin/models/Character.js'
+import CharImg from '../../miao-plugin/models/character/CharImg.js'
+import ProfileChange from '../../miao-plugin/apps/profile/ProfileChange.js'
 import LlmMeta from './LlmMeta.js'
 
 const pluginName = 'lolomi-calc'
@@ -71,6 +75,8 @@ let LlmDataIndex = {
       miaoChar.addData(missingData)
       miaoChar.addAlias(missingAlias)
     }
+
+    LlmDataIndex.patchCharFallback()
   },
 
   async regWeapon () {
@@ -135,6 +141,155 @@ let LlmDataIndex = {
       weaponBuffs: lodash.assign({}, miaoWeaponCfg.weaponBuffs, weaponBuffs),
       descFix: lodash.assign({}, miaoWeaponCfg.descFix, descFix)
     })
+
+    LlmDataIndex.patchWeaponFallback()
+    LlmDataIndex.patchMatchMsg(alias)
+  },
+
+  /**
+   * 面板变换功能的冲突处理，例如 7.1薇斯纳专武 蝶变
+   */
+  patchMatchMsg (alias) {
+    if (LlmDataIndex._matchPatched) {
+      return
+    }
+    LlmDataIndex._matchPatched = true
+
+    let words = []
+    lodash.forEach(alias, (v, k) => {
+      lodash.forEach(`${k},${v}`.split(','), (s) => {
+        s = lodash.trim(s)
+        if (/[变改]/.test(s) && !words.includes(s)) {
+          words.push(s)
+        }
+      })
+    })
+    words.sort((a, b) => b.length - a.length)
+    if (!words.length) {
+      return
+    }
+
+    const SAFE = '旅行剑'
+    const orig = ProfileChange.matchMsg
+    ProfileChange.matchMsg = function (msg) {
+      let hit
+      for (const w of words) {
+        if (msg.includes(w)) {
+          hit = w
+          msg = msg.split(w).join(SAFE)
+          break
+        }
+      }
+      let ret = orig.call(this, msg)
+      if (hit && ret && ret.change && ret.change.weapon && ret.change.weapon.weapon === SAFE) {
+        ret.change.weapon.weapon = hit
+      }
+      return ret
+    }
+  },
+
+  patchCharFallback () {
+    if (LlmDataIndex._charPatched) {
+      return
+    }
+    LlmDataIndex._charPatched = true
+
+    const miaoRoot = Data.getRoot('miao')
+    const llmChar = (name, f) => `${pluginRoot}/${resPath}/character/${name}/${f}`
+    const relChar = (name, f) => `../../${pluginName}/${resPath}/character/${name}/${f}`
+
+    const origGetDetail = Character.prototype.getDetail
+    Character.prototype.getDetail = function () {
+      let ret = origGetDetail.call(this)
+      if ((!ret || !ret.attr) && this.isGs && !this.isTraveler && this.name && fs.existsSync(llmChar(this.name, 'data.json'))) {
+        ret = Data.readJSON(`${resPath}/character/${this.name}/data.json`, pluginName)
+        if (ret && ret.attr) {
+          this.meta = this.meta || {}
+          this.meta._detail = ret
+        }
+      }
+      return ret
+    }
+
+    const origGetImgs = CharImg.getImgs
+    CharImg.getImgs = function (name, ...args) {
+      let imgs = origGetImgs.call(this, name, ...args)
+      const prefix = `/meta-gs/character/${name}/`
+      lodash.forEach(imgs, (v, k) => {
+        if (lodash.isString(v) && v.startsWith(prefix)) {
+          let sub = v.slice(prefix.length)
+          if (!fs.existsSync(`${miaoRoot}resources${v}`) && fs.existsSync(llmChar(name, sub))) {
+            imgs[k] = relChar(name, sub)
+          }
+        }
+      })
+      return imgs
+    }
+  },
+
+  patchWeaponFallback () {
+    if (LlmDataIndex._weaponPatched) {
+      return
+    }
+    LlmDataIndex._weaponPatched = true
+
+    const miaoRoot = Data.getRoot('miao')
+    const miaoFile = (w, f) => `${miaoRoot}resources/meta-gs/weapon/${w.type}/${w.name}/${f}`
+    const llmFile = (w, f) => `${pluginRoot}/${resPath}/weapon/${w.type}/${w.name}/${f}`
+    const relFile = (w, f) => `../../${pluginName}/${resPath}/weapon/${w.type}/${w.name}/${f}`
+    const iconCache = {}
+    const useLlmIcon = (w) => {
+      let key = `${w.type}/${w.name}`
+      if (lodash.isUndefined(iconCache[key])) {
+        iconCache[key] = !!(w.isGs && w.type && w.name &&
+          !fs.existsSync(miaoFile(w, 'icon.webp')) && fs.existsSync(llmFile(w, 'icon.webp')))
+      }
+      return iconCache[key]
+    }
+
+    const origGetDetail = Weapon.prototype.getDetail
+    Weapon.prototype.getDetail = function () {
+      let ret = origGetDetail.call(this)
+      if ((!ret || !ret.attr) && this.isGs && this.type && this.name && fs.existsSync(llmFile(this, 'data.json'))) {
+        ret = Data.readJSON(`${resPath}/weapon/${this.type}/${this.name}/data.json`, pluginName)
+        if (ret) {
+          this._detail = ret
+        }
+      }
+      return ret
+    }
+
+    const origImg = Object.getOwnPropertyDescriptor(Weapon.prototype, 'img').get
+    Object.defineProperty(Weapon.prototype, 'img', {
+      get () {
+        return useLlmIcon(this) ? relFile(this, 'icon.webp') : origImg.call(this)
+      }
+    })
+    const origImgs = Object.getOwnPropertyDescriptor(Weapon.prototype, 'imgs').get
+    Object.defineProperty(Weapon.prototype, 'imgs', {
+      get () {
+        if (!useLlmIcon(this)) {
+          return origImgs.call(this)
+        }
+        return {
+          icon: relFile(this, 'icon.webp'),
+          icon2: relFile(this, 'awaken.webp'),
+          gacha: relFile(this, 'gacha.webp')
+        }
+      }
+    })
+
+    const origGet = Weapon.get
+    Weapon.get = function (name, game = 'gs', type = '') {
+      let ret = origGet.call(Weapon, name, game, type)
+      if (!ret && (!game || game === 'gs')) {
+        let data = LlmMeta.getData('gs', 'weapon', name)
+        if (data && data.name) {
+          ret = new Weapon(data, 'gs')
+        }
+      }
+      return ret
+    }
   },
 
   async regArti () {
